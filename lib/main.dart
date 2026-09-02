1dcb9962c65433b2d758dc685ccbac666897de67
Felipe Bueno <felipehbs26@gmail.com>
2026-09-02 15:10:43 -0300

    Organiza tela inicial em arquivo separado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'telas/tela_inicial.dart';
 
 void main() {
   runApp(const BibliotecaApp());
@@ -9,58 +10,10 @@ class BibliotecaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Biblioteca Escolar',
       debugShowCheckedModeBanner: false,
       home: const TelaInicial(),
-    );
-  }
-}
-
-class TelaInicial extends StatelessWidget {
-  const TelaInicial({super.key});
-
-  @override
-  Widget build(BuildContext) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Biblioteca Escolar'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(
-              Icons.local_library,
-              size: 80,
-            ),
-            const SizedBox(height: 20),
-
-            const Text(
-              'Sistema da Biblioteca',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            const SizedBox(height: 40),
-            
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Alunos'),
-            ),
-
-            const SizedBox(height: 15),
-            
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Empréstimos'),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
